9aec09fd01c1399ace8b3c1030e26f2a1b9d1bfb
carlosandrews47 <carlosandrecamposdesousaa@gmail.com>
2025-11-10 20:57:03 -0300

    Add files via upload

---
+++ Aulaspython/src/aula.py
@@ -0,0 +1,33 @@
+#calculadora
+"""
+
+numero1 = 5
+numero2 = 2
+#soma
+print(numero1 + numero2)
+#subtração
+print(numero1 - numero2)
+#Multiplicação
+print(numero1 * numero2)
+#Divisão
+print(numero1 / numero2)
+#módulo
+print(numero1 %2)
+"""
+
+
+#Saida de dados
+nome = "Astrogenildo"
+idade = 30
+peso = 60.0
+print ("Bem vindo ao nosso programa")
+print("{} tem {} anos de idade e pesa {:.2f} kg".format (nome, idade, peso))
+
+nome = input ("digite um nome: ")
+print(nome)
+idade = int (input ("digite sua idade: "))
+peso = float (input ("digite o peso: "))
+print(nome)
+print(idade)
+print(peso)
+print (f"{nome} tem {idade} anos de idade e pesa {peso:.3f} kg")
